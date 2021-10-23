5c90b4c8c496a75f10db18b28df6f64141d0149b
lugerbrown <luismarron@gmail.com>
2021-10-23 14:59:33 +0100

    Added file_list view.

--- rest_api/file_manager/urls.py
+++ rest_api/file_manager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from .views import file
+from .views import file, files_list
 
 urlpatterns = [
     path('documents/<str:name>/<int:revision>', file, name="file"),
+    path('documents', files_list, name="file_list"),
 ]
