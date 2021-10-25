025327dc4b8b56de40fe59534720dafd35585676
lugerbrown <luismarron@gmail.com>
2021-10-25 13:19:02 +0100

    Added Assertions.

--- web-app/test/unit/file.spec.js
+++ web-app/test/unit/file.spec.js
@@ -12,7 +12,8 @@ describe('File Component',() => {
       .boundTo({
         document:{
           name:'FIleName.txt',
-          downloadUrl:'http://localhost:8000/download'
+          downloadUrl:'http://localhost:8000/download',
+          revision:1
         }});
   });
 
@@ -22,10 +23,12 @@ describe('File Component',() => {
     .then(() => component.attached())
     .then(() => component.bind())
     .then(() => {            
-      var titleElement = document.querySelector('.fw-bold')
-      var downloadLink = document.querySelector('a.delete-link')
+      var titleElement = document.querySelector('.fw-bold');
+      var downloadLink = document.querySelector('a.delete-link');
+      var revisionBadge =  document.querySelector('span.badge');
       expect(titleElement.innerHTML).toBe('FIleName.txt');
       expect(downloadLink.getAttribute("href")).toBe('http://localhost:8000/download');      
+      expect(revisionBadge.innerHTML).toBe('Rev.1');      
     })
     .then(done);
   });
